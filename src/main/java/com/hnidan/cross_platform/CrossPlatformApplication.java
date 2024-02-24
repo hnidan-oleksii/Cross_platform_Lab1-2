8f0e3458042f158dd7e26d03adc1095ba77bb42b
surikadt <gnidanol@gmail.com>
2024-02-24 09:46:52 +0200

    Initial commit

---
+++ src/main/java/com/hnidan/cross_platform/CrossPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.hnidan.cross_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hnidan/cross_platform/CrossPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnidan.cross_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
